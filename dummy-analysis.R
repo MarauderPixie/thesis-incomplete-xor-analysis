87e0368509f908746e61d9d2c042d39862063481
MarauderPixie <tobias.anton.85@gmail.com>
2022-04-28 13:55:01 +0200

    backup, sorta

--- dummy-analysis.R
+++ dummy-analysis.R
@@ -1,23 +1,31 @@
-### conditions as written on a napkin:
+library(tidyverse)
+library(brms)
+
+### condition-assignment:
 # 1: no rll, no ordering
 # 2: no rr, simple rule first
 # 3: rll, no ordering
 # 4: rll, srf
 
-dummy <- read_csv("Downloads/results_18_incomplete-xor-testrun-3_Tobi+A..csv") %>% 
+
+# the data used here was collected in several developmental stages of the experiment;
+# final results will look very similar in structure such that this example should
+# completely suffice to serve as an example
+dummy <- read_csv("data-raw/messy-dummy-data.csv") %>% 
   mutate(
     assignment = case_when(assignment == "1" ~ "correct1",
                            assignment == "2" ~ "correct2",
                            TRUE ~assignment)
   )
 
-# untrained trials:
+# incomplete category images:
 utt <- paste0(
   "images/", c("e45_4000-366.7.jpg", "e44_4000-233.3.jpg", "e43_4000-100.jpg",
-                "e38_3583-366.7.jpg", "e37_3583-233.3.jpg", "e36_3583-100.jpg",
-                "e31_3167-366.7.jpg", "e30_3167-233.3.jpg", "e29_3167-100.jpg")
-  )
+               "e38_3583-366.7.jpg", "e37_3583-233.3.jpg", "e36_3583-100.jpg",
+               "e31_3167-366.7.jpg", "e30_3167-233.3.jpg", "e29_3167-100.jpg")
+)
 
+# data prep
 train <- dummy %>% 
   filter(!is.na(correct1)) %>% 
   mutate(
@@ -28,6 +36,20 @@ train <- dummy %>%
   ) %>% 
   select(-correct1, -correct2, -education, -gender, -submission_id)
 
+aggr <- train %>% 
+  group_by(subj, condition, block) %>% 
+  summarise(
+    acc = mean(response == correct)
+  )
+
+aggr_binom <- train %>% 
+  group_by(subj, condition, block) %>% 
+  summarise(
+    k = sum(response == correct),
+    n = n()
+  )
+
+
 general <- dummy %>% 
   filter(image %in% utt) %>% 
   mutate(
@@ -37,22 +59,25 @@ general <- dummy %>%
                      response == "B" & assignment == "correct2" ~ 0,
                      TRUE ~ 1) # needs to be validated very very much!!
   ) %>% 
-  select(-correct1, -correct2, -education, -gender,
-         -submission_id, -assignment)
+  select(subj, condition, exin)
 
-
-aggr <- train %>% 
-  group_by(subj, condition, block) %>% 
+general_binom <- general %>% 
+  group_by(subj, condition) %>% 
   summarise(
-    acc = mean(response == correct)
+    k = sum(exin),
+    n = n()
   )
 
 
-library(brms)
-accmod1 <- brm(data = aggr, acc ~ block + (block|subj), iter = 2000)
-accmod2 <- brm(data = aggr, acc ~ block + (1|subj), iter = 2000)
-accmod3 <- brm(data = aggr, acc ~ condition * block + (1 | subj + block), iter = 2000)
 
 
-binmod1 <- brm(data = general, exin ~ condition + (1|subj), family = binomial)
-binmod2 <- brm(data = general, exin ~ condition + (1|subj), family = bernoulli)
+# final models, approximately:
+model_training <- brm(data = aggr, acc ~ condition * block + (1|subj + block))
+model_general  <- brm(data = general, exin ~ condition + (1|subj), family = bernoulli)
+
+alt_model_training <- brm(data = aggr_binom, 
+                          k|trials(n) ~ condition * block + (1|subj + block), 
+                          family = binomial)
+alt_model_general  <- brm(data = general_binom, 
+                          k|trials(n) ~ condition + (1|subj), 
+                          family = binomial)

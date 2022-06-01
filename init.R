4c312f3e4fe82abb6e01d37a2f563d21654788d7
MarauderPixie <tobias.anton.85@gmail.com>
2022-06-01 15:59:04 +0200

    if nothing else, at least my plots look nice and fit my rstudio

--- init.R
+++ init.R
@@ -3,6 +3,9 @@ library(brms)
 # library(bayesplot)
 # library(loo)
 
+theme_set(hrbrthemes::theme_ipsum_ps(plot_margin = margin(10, 10, 10, 10)) + 
+            theme(panel.background = element_rect(fill = "#f0f0f0"),
+                  plot.background = element_rect(fill = "#f0f0f0")))
 ncore <- parallel::detectCores()
 
 simt <- readRDS("data-raw/simulated_training.rds")

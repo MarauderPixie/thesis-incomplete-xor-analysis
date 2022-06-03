dfa57e3550a7a4245785dc0e32c549feb1ba4a16
MarauderPixie <tobias.anton.85@gmail.com>
2022-06-03 17:44:23 +0200

    have the data simulation seperately

---
+++ data-simulation.R
@@ -0,0 +1,48 @@
+n_subj <- 200
+
+subj  <- paste0("S", str_pad(seq(n_subj), 2, pad = "0")) %>% as_factor()
+cond  <- rep(LETTERS[1:4], each = n_subj/4) %>% as_factor()
+block <- as_factor(1:12)
+imgs1 <- rep(paste0("img", 1:4), 2)
+imgs2 <- c(paste0("img", 1:6), imgs1[1:2])
+# gimgs <- paste0("img", str_pad(1:49, 2, pad = "0"))
+gen_state <- c(rep("critical", 9), rep("midlane", 13), rep("regular", 27))
+
+simdat <- tibble(
+  subj = rep(subj, each = 96),
+  condition = rep(cond, each = 96),
+  block = rep(block, each = 8, times = n_subj),
+  image = c(
+    replicate((n_subj/4)*12, sample(imgs2)) %>% as.vector(),
+    replicate((n_subj/4), c(replicate(2, sample(imgs1)), 
+                            replicate(10, sample(imgs2)))) %>% as.vector(),
+    replicate((n_subj/4)*12, sample(imgs2)) %>% as.vector(),
+    replicate((n_subj/4), c(replicate(2, sample(imgs1)),
+                            replicate(10, sample(imgs2)))) %>% as.vector()
+  ) %>% factor(),
+  correct = c(
+    map(replicate((n_subj/4), rbeta(12, 3, 6) %>% sort()) %>% as.vector(), ~rbernoulli(n = 8, p = .x)) %>% flatten_int(),
+    map(replicate((n_subj/4), rbeta(12, 4, 5) %>% sort()) %>% as.vector(), ~rbernoulli(n = 8, p = .x)) %>% flatten_int(),
+    map(replicate((n_subj/4), rbeta(12, 5, 4) %>% sort()) %>% as.vector(), ~rbernoulli(n = 8, p = .x)) %>% flatten_int(),
+    map(replicate((n_subj/4), rbeta(12, 6, 3) %>% sort()) %>% as.vector(), ~rbernoulli(n = 8, p = .x)) %>% flatten_int()
+  ) # ,
+  # rt = c(
+  #   map(rnorm(25, 220, 20), ~rnorm(n = 8, mean = 2200 - .x, sd = 250)) %>% flatten_dbl()
+  # )
+)
+
+gensim <- tibble(
+  subj = rep(subj, each = 49),
+  condition = rep(cond, each = 49),
+  image = replicate(n_subj, sample(gen_state, 49)) %>% as.vector(),
+  response = c( # taking it easy...
+    replicate((n_subj/4)) %>% as.integer(),
+    replicate((n_subj/4)) %>% as.integer(),
+    replicate((n_subj/4)) %>% as.integer(),
+    replicate((n_subj/4), rbernoulli(49, rbeta(1, 4, 2))) %>% as.integer()
+  )
+)
+rm(subj, cond, block, imgs1, imgs2, gen_state)
+
+saveRDS(simdat, "data-raw/simulated_training.rds")
+saveRDS(gensim, "data-raw/simulated_generalization.rds")
